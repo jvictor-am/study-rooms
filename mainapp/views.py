c5c50152faeef0c92bd90e0e0284db32c7269859
jvictor-am <joaovictorengp@gmail.com>
2021-10-02 16:49:32 -0300

    changes templates structure & urls

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Python for begginers'},
+    {'id': 2, 'name': 'AWS & DevOps'},
+    {'id': 3, 'name': 'React Expert'},
+]
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'mainapp/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for each_room in rooms:
+        if each_room['id'] == int(pk):
+            room = each_room
+    context = {'room': room}
+
+    return render(request, 'mainapp/room.html', context)
