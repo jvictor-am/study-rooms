c2fce3c3558c8c08cfa55d66806c112eb18d1b28
jvictor-am <joaovictorengp@gmail.com>
2021-10-02 19:43:24 -0300

    creates models & adjust admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Room, Topic, Message
 
-# Register your models here.
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'host', 'topic', 'updated')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'body', 'user', 'room', 'updated')
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,20 +1,15 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'Python for begginers'},
-    {'id': 2, 'name': 'AWS & DevOps'},
-    {'id': 3, 'name': 'React Expert'},
-]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'mainapp/home.html', context)
 
+
 def room(request, pk):
-    room = None
-    for each_room in rooms:
-        if each_room['id'] == int(pk):
-            room = each_room
+    room = Room.objects.get(id=pk)
     context = {'room': room}
 
     return render(request, 'mainapp/room.html', context)
